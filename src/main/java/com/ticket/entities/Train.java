3edbd51e6d084cc00ceb1889bc319c08e8c4eaf4
imam-007 <imimam8409@gmail.com>
2025-05-19 22:30:14 +0530

    added entities

---
+++ src/main/java/com/ticket/entities/Train.java
@@ -0,0 +1,18 @@
+package com.ticket.entities;
+
+import java.util.List;
+import java.util.Map;
+
+public class Train {
+    private String trainId;
+
+    private String trainNo;
+
+    private List<List<Integer>> seats;
+
+    private Map<String, String> stationTime;
+
+    private List<String> stations;
+
+
+}
---
+++ src/main/java/com/ticket/entities/User.java
@@ -0,0 +1,15 @@
+package com.ticket.entities;
+
+import java.util.List;
+
+public class User {
+    private String name;
+
+    private String userId;
+
+    private String password;
+
+    private String hashPassword;
+
+    private List<Ticket> ticketsBooked;
+}
